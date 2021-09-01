01f816badd6f841457eef932be01f9f3a1a8556b
Vũ Đình Hữu <huuotis@gmail.com>
2021-09-01 17:11:05 +0700

    scrollreveal

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -2,35 +2,35 @@ import React from "react";
 import Header from "../../components/Header";
 import Body from "../../components/Body";
 import Footer from "../../components/Footer";
-// import sr from "../../ScrollReveal";
+import sr from "../../ScrollReveal";
 
 export class HomePage extends React.Component {
-  // componentDidMount = () => {
-  //   const config = {
-  //     distance: "30px",
-  //     duration: 1800,
-  //     reset: true
-  //   }
-  //   sr.reveal(
-  //     `.home__data, .home__img, 
-  //              .decoration__data,
-  //              .accessory__content,
-  //              .footer__content`,
-  //     {
-  //       ...config,
-  //       origin: "top",
-  //       interval: 200
-  //     }
-  //   );
-  //   sr.reveal(`.share__img, .send__content`, {
-  //     ...config,
-  //     origin: "left",
-  //   });
-  //   sr.reveal(`.share__data, .send__img`, {
-  //     ...config,
-  //     origin: "right"
-  //   })
-  // }
+  componentDidMount = () => {
+    const config = {
+      distance: "30px",
+      duration: 1800,
+      reset: true
+    }
+    sr.reveal(
+      `.home__data, .home__img, 
+               .decoration__data,
+               .accessory__content,
+               .footer__content`,
+      {
+        ...config,
+        origin: "top",
+        interval: 200
+      }
+    );
+    sr.reveal(`.share__img, .send__content`, {
+      ...config,
+      origin: "left",
+    });
+    sr.reveal(`.share__data, .send__img`, {
+      ...config,
+      origin: "right"
+    })
+  }
   
   render() {
     return (
